a47cfe022f4de3ef5525062e110ee14c3e6753c0
DinjerChang <tingtsechang@gmail.com>
2021-10-19 18:34:21 +0800

    delete feature

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const exphbs = require('express-handlebars')
 const port = 3000
 const restaurantList = require('./restaurant.json')
+const Restaurant = require('./models/restaurant') //load restaurant model
 
 // step 2 : set template engine & static file
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -13,33 +14,74 @@ app.set('view engine', 'handlebars')
 // setting static files
 app.use(express.static('assets'))
 
-// step 3 : set dynamic route
-app.get('/', (req, res) => {
-  res.render('index', { restaurants: restaurantList.results })
-})
-
 // setup database connection
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/restaurant-list')
 const db = mongoose.connection
-db.on('error',() =>{
+db.on('error', () => {
   console.log('error')
 })
-db.once('open',() => {
+db.once('open', () => {
   console.log('mongo connected')
+})
+
+//setup body-parser
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: true })) //"use" 屬性要求每筆請求經過 body-parser 先處理 
+
+// step 3 : set dynamic route
+app.get('/', (req, res) => {
+  return Restaurant.find().lean().then(restaurants => (res.render('index', { restaurants: restaurants }))).catch(err => console.log(err))
 })
 
 // show restaurant page deatail 
 app.get('/restaurants/:restaurant_id', (req, res) => {
-  const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
-  res.render('show', { restaurant: restaurant })
+  // const restaurant = restaurantList.results.find(restaurant => restaurant.id.toString() === req.params.restaurant_id)
+  return Restaurant.findById(req.params.restaurant_id).lean().then((restaurant) => { res.render('show', { restaurant: restaurant }) }).catch(err => console.log(err));
 })
+
 // search function
 app.get('/search', (req, res) => {
   const keywords = req.query.keyword.toLowerCase().trim()
-  const restaurants = restaurantList.results.filter(restaurant => restaurant.name.toLowerCase().includes(keywords) || restaurant.category.toLowerCase().includes(keywords));
-  res.render('index', { restaurants: restaurants, keyword: keywords})
+  Restaurant.find().lean().then(restaurants => { res.render('index', { restaurants: restaurants.filter(restaurant => restaurant.name.toLowerCase().includes(keywords) || restaurant.category.toLowerCase().includes(keywords)), keyword: keywords }) }).catch(err => console.log(err))
 })
+// add new restaurant feature
+app.get('/restaurant/new', (req, res) => {
+  res.render('new')
+})
+app.post('/restaurants', (req, res) => {
+  const new_res = req.body
+  return Restaurant.create(new_res).then(() => res.redirect('/')).catch(err => console.error(err))
+})
+
+// add edit feature
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  Restaurant.findById(id).lean().then(restaurant => res.render('edit', { restaurant })).catch(err => console.error(err))
+})
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const edit_res = req.body
+  return Restaurant.findById(id).then(restaurant => {
+    restaurant.name = edit_res.name;
+    return restaurant.save()
+  }).then(() => {
+    res.redirect(`/restaurants/${id}`)
+  }).catch(err => console.log(err))
+})
+
+// add delete feature
+app.post('/restaurants/:id/delete', (req, res) => {
+  console.log(req.params)
+  const id = req.params.id
+  return Restaurant.findById(id).then(restaurant => {
+    restaurant.remove()
+    res.redirect('/')
+  }).catch(err => {
+    console.log(err)
+  })
+})
+
 app.listen(port, () => {
   console.log('this is your restaruant website by express')
 })
--- models/seeds/restaurantSeeder.js
+++ models/seeds/restaurantSeeder.js
@@ -10,7 +10,7 @@ db.once('open', () => {
   console.log('db connected')
   for (let i = 0; i < restaurantList.results.length; i++) {
     const list = restaurantList.results[i]
-    Restaurant.create({ name : list.name, category : list.category, image : list.image, location : list.location, phone : list.phone, google_map : list.google_map, rating : list.rating, description : list.description})
+    Restaurant.create({ id : list.id, name : list.name, category : list.category, image : list.image, location : list.location, phone : list.phone, google_map : list.google_map, rating : list.rating, description : list.description})
 }})
 
 console.log(restaurantList.results.length)
